5b83b875d9462845ed4e41489f2a4440763f2fb7
Your Name <you@example.com>
2023-08-10 09:24:29 +0300

    kotlin work

---
+++ src/main/kotlin/DataTypes.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+
+    var age:Int= 23 // Integer
+    var country:String = "kenya" // String
+    var letter:Char = 'k' // Char
+    var myBoolean:Boolean = true
+    var temperature:Double = 37.5 // Double
+    var points:Float =40.578915F
+}
---
+++ src/main/kotlin/simplebmi.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+
+    println("Enter your height in cm: ")
+    var num1: Int = readLine()!!.toInt()
+    println("Enter your weight in kg: ")
+    var num2: Int = readLine()!!.toInt()
+
+
+    var result = num1 /(num2 /100) *2
+
+    if (result <= 18){
+        print("Underweight")
+    }
+    else if (result <= 29){
+        print("Normal weight")
+    }
+    else if (result <= 34){
+        print("overweight")
+    }
+    else if (result <= 34){
+        print("Obese")
+    }
+    print(result)
+}
