7a3e5eaa2272dd85bcd320e3250b7a7a6eaffc9f
Your Name <you@example.com>
2023-08-14 10:32:00 +0300

    kotlin work

---
+++ src/main/kotlin/functions.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+
+
+
+    printSum(120,  500)
+}
+
+fun printSum(num1: Int, num2: Int) {
+    val sum = sumNumbers(num1, num2)
+    print(sum)
+}
+fun sumNumbers(num1:Int , num2:Int):Int{
+    return num1 + num2
+
+}
+
+
+
+
---
+++ src/main/kotlin/functionswith parametersex.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+
+    println("Enter first number")
+    var num1 = readLine()!!.toInt()
+
+    println("Enter second num")
+    var num2:Int = readLine()!!.toInt()
+
+    printsum(num1,num2)
+}
+
+
+fun printsum(num1: Int, num2: Int) {
+    val sum = sumnumbers(num1, num2)
+    print(sum)
+}
+fun sumnumbers(num1:Int , num2:Int):Int {
+    return num1 + num2
+}
